9aec62ceeb3b2b30cbb91d9eacc0c5fbacc035fc
MHryc <96656809+MHryc@users.noreply.github.com>
2025-03-13 02:02:49 +0100

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -4,22 +4,16 @@
 #mkdir ~/pl0217-01/project_data/containers_hryc
 #ln -s ~/pl0217-01/project_data/containers_hryc/ .singularity/
 
-#cd ~/pl0217-01/project_data/4_MHryc/containers/container_test
-
-#DIR=$(readlink -f ~/pl0217-01/project_data)
-#export WORK_DIR="4_MHryc/containers/container_test"
-export WORK_DIR=$(readlink -f .)
-
 chmod +x scripts/*
 
 mkdir -p results/fastqc_out results/fastp_out
 
-# Pobranie danych WGS Streptococcus pyogenes
+export WORK_DIR=$(readlink -f .)
 export RUN_ID="SRR32524951"
 NCBI_REF="streptococus_ncbi.txt"
-SRA_TOOLS="https://depot.galaxyproject.org/singularity/sra-tools:3.2.0--h4304569_0"
-#wget -nc -i $NCBI_REF -P ncbi_files
+wget -nc -i $NCBI_REF -P ncbi_files
 
+SRA_TOOLS="https://depot.galaxyproject.org/singularity/sra-tools:3.2.0--h4304569_0"
 singularity exec \
 	--bind "$WORK_DIR:/mnt/proj" \
 	$SRA_TOOLS bash -c /mnt/proj/scripts/sratools.sh
@@ -30,7 +24,10 @@ singularity exec \
 	$FASTQC bash -c /mnt/proj/scripts/fastqc.sh
 
 FASTP="https://depot.galaxyproject.org/singularity/fastp:0.24.0--heae3180_1"
-
 singularity exec \
 	--bind "$WORK_DIR:/mnt/proj" \
 	$FASTP bash -c /mnt/proj/scripts/fastp.sh
+
+singularity exec \
+	--bind "$WORK_DIR:/mnt/proj" \
+	$FASTP bash -c /mnt/proj/scripts/fastqc_post_fastp.sh
