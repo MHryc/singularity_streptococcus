e64624cd76e71bb65ad361fa70e090339f8a016f
MHryc <96656809+MHryc@users.noreply.github.com>
2025-03-13 02:16:10 +0100

    Update fastp.sh

--- scripts/fastp.sh
+++ scripts/fastp.sh
@@ -2,8 +2,8 @@
 
 cd /mnt/proj/$RUN_ID
 
-fastp -i ${RUN_ID}_1.fastq -o ${RUN_ID}_1.fastp.fastq \
-	-I ${RUN_ID}_2.fastq -O ${RUN_ID}_2.fastp.fastq \
+fastp -i ${RUN_ID}_1.fastq -o ${RUN_ID}_1.fastp.fastq -f 10 \
+	-I ${RUN_ID}_2.fastq -O ${RUN_ID}_2.fastp.fastq -F 10 \
 	-q 30 -u 40 -l 35 --trim_poly_x -z 4 --cut_tail \
 	--cut_tail_window_size 1 --cut_tail_mean_quality 20 \
 	--adapter_fasta /mnt/proj/resources/adapter.fa \
