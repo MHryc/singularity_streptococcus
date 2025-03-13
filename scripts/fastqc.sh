af9bc98a3a1cac9d1e4c67bd2f67dfab69a2ea94
MHryc <96656809+MHryc@users.noreply.github.com>
2025-03-13 01:48:19 +0100

    Update fastqc.sh

--- scripts/fastqc.sh
+++ scripts/fastqc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /mnt/proj/$RUN_ID
-fastqc -t 2 ${RUN_ID}_1.fastq ${RUN_ID}_2.fastq -o ../results/fastqc_out
+fastqc -t 2 ${RUN_ID}_1.fastq ${RUN_ID}_2.fastq -o /mnt/proj/results/fastqc_out
